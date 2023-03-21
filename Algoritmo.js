b39acbdaa5e2b79fbd099e91dd88a665da91930a
Noelia, Johanna, Nahir <prueba@gmail.com>
2023-03-21 12:02:08 -0300

    primer commit

---
+++ Algoritmo.js
@@ -0,0 +1,20 @@
+document.querySelector("body").innerHTML = ` <iframe width="560" height="315" src="https://www.youtube.com/embed/ivdTnPl1ND0" title="YouTube video player"
+frameborder="0"
+allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+allowfullscreen></iframe>
+<p>Esta descripcion es de un video tutorial de Java Script</p>
+
+<iframe
+src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13813759.264619159!2d-64.79381004441198!3d-32.29842315875966!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9575073afb5fde09%3A0x4a5596616016524a!2sUruguay!5e0!3m2!1ses!2sar!4v1679403562387!5m2!1ses!2sar"
+width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+referrerpolicy="no-referrer-when-downgrade"></iframe>
+<p>Ubicación de Uruguay</p>
+
+<iframe style="border-radius:12px"
+src="https://open.spotify.com/embed/track/04ndZkbKGthTgYSv3xS7en?utm_source=generator" width="100%" height="352"
+frameBorder="0" allowfullscreen=""
+allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+<p>Cancion de Tini</p>
+
+<iframe height="500px" width="1000px" src="https://www.mercadolibre.com.ar/" frameborder="0"></iframe>
+<p>Pagina de productos</p> ` ;
